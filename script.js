2c55624c1d3a519cac850c32d63bc5d334528c37
Yajzkie <yaj143sabalo@gmail.com>
2026-08-17 19:14:35 +0800

    fix navbar

--- script.js
+++ script.js
@@ -139,3 +139,35 @@ document.addEventListener(
 
     }
 );
+
+/* =========================================================
+   MOBILE NAVIGATION
+========================================================= */
+
+const menuToggle =
+    document.getElementById("menuToggle");
+
+const navMenu =
+    document.getElementById("navMenu");
+
+
+menuToggle.addEventListener("click", function () {
+
+    navMenu.classList.toggle("active");
+
+});
+
+
+const navLinks =
+    document.querySelectorAll("#navMenu a");
+
+
+navLinks.forEach(function (link) {
+
+    link.addEventListener("click", function () {
+
+        navMenu.classList.remove("active");
+
+    });
+
+});
